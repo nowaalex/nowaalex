47cbbfb89a059b3237336b1de9ea68fa563ae472
alex <nowaalex@gmail.com>
2024-05-23 04:07:07 +0200

    added tray and powermenu to i3 config

--- configs/.config/polybar/launch.sh
+++ configs/.config/polybar/launch.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-
 polybar-msg cmd quit
-polybar mainbar & disown
+
+echo "---" | tee -a /tmp/polybar-mainbar.log
+polybar mainbar 2>&1 | tee -a /tmp/polybar-mainbar.log & disown
+echo "Bars launched..."
+
