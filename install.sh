40de474a8d0319d97d5b1cef2762ddcd991309a7
alex <nowaalex@gmail.com>
2024-06-29 21:01:02 +0200

    modified waybar

--- install.sh
+++ install.sh
@@ -10,3 +10,7 @@ curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmo
 echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt update
 sudo apt install -y google-chrome-stable
+
+# /etc/environment 
+# LIBVA_DRIVER_NAME=iHD
+# VDPAU_DRIVER=va_gl
