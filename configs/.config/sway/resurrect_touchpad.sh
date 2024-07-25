50bedaafc3fd7c00132231e049e4ba2197f39074
alex <nowaalex@gmail.com>
2024-07-26 01:40:24 +0200

    modified resurrect_touchpad

--- configs/.config/sway/resurrect_touchpad.sh
+++ configs/.config/sway/resurrect_touchpad.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo modprobe -r i2c_hid_acpi
+sudo modprobe -r i2c_hid
 sudo modprobe i2c_hid_acpi
+sudo modprobe i2c_hid
 notify-send -t 2000 -c system -u low "Touchpad restarted" "i2c_hid_acpi toggled"
