c82293c56a48975f3ead649ec9f3074f2c3a7714
alex <nowaalex@gmail.com>
2024-08-16 03:52:05 +0200

    added bindsym --to-code

--- configs/.config/sway/resurrect_touchpad.sh
+++ configs/.config/sway/resurrect_touchpad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo modprobe -r i2c_hid_acpi
-sudo modprobe -r i2c_hid
-sudo modprobe i2c_hid_acpi
-sudo modprobe i2c_hid
+modprobe -r i2c_hid_acpi
+modprobe -r i2c_hid
+modprobe i2c_hid_acpi
+modprobe i2c_hid
 notify-send -t 2000 -c system -u low "Touchpad restarted" "i2c_hid_acpi toggled"
