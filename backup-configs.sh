3da899b1ded6c554330ad14f09856821c0f5d6fc
Alex Fomin <magritte@tutamail.com>
2025-02-15 04:46:52 +0100

    added bashrc to copied configs

--- backup-configs.sh
+++ backup-configs.sh
@@ -1,6 +1,7 @@
 cd configs
 rm -rfv ./config/*
 cp ~/.bash_profile .
+cp ~/.bashrc .
 cp ~/.gitconfig .
 cp -r ~/.config/{sway,fuzzel,waybar,wlogout,dconf,foot,mimeapps.list} .config
 cp -r ~/wallpapers .
