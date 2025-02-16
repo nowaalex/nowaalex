ca8ab2d02a40ce443312af78265428baabcc53e5
Alex Fomin <magritte@tutamail.com>
2025-02-16 12:46:50 +0100

    minor todos

--- backup-configs.sh
+++ backup-configs.sh
@@ -3,5 +3,8 @@ rm -rfv ./config/*
 cp ~/.bash_profile .
 cp ~/.bashrc .
 cp ~/.gitconfig .
-cp -r ~/.config/{sway,fuzzel,waybar,wlogout,dconf,foot,mimeapps.list} .config
+cp -r ~/.config/{sway,fuzzel,waybar,wlogout,dconf,alacritty,mimeapps.list} .config
 cp -r ~/wallpapers .
+
+# TODO: /etc/vconsole.conf FONT setting
+# TODO: save yay -Qeq output (manually installed packages)
